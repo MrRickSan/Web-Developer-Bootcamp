1d97b3f8029b2f3cf86a1b77f60f111235809d5d
DESKTOP-0D0D8FB\Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-05-05 02:02:18 -0300

    javascript intro

---
+++ section8 - javascript/exercises/93 - js satlker exercise/js-stalker.js
@@ -0,0 +1,8 @@
+/* global prompt */
+
+let first = prompt('Input your first name: ')
+let last = prompt('Now input your last name :')
+let age = prompt('Now your age: ')
+
+console.log(`Your full name is ${first} ${last}`)
+console.log(`You are ${age} years old`)
---
+++ section8 - javascript/exercises/95 - age calculator exercise/age-calculator.js
@@ -0,0 +1,5 @@
+/* global prompt alert */
+
+let age = prompt('How old are you?')
+
+alert(`${age} is roughly ${age * 365} days`)
