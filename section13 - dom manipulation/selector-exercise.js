e5b2c8c352df8e4e7641303a230590feef6f9576
Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-05-28 17:12:20 -0300

    dom manipulation

---
+++ section13 - dom manipulation/selector-exercise.js
@@ -0,0 +1,23 @@
+var first = $('#first')
+var specialClass = $('.special')
+var allSpecialClasses = $$('.special')
+var allPTags = $$('p')
+
+
+// get the first p tag
+var pTag = $('p')
+var pTags = $$('p')[0]
+
+
+// video
+document.getElementById('first')
+document.querySelector('#first')
+document.getElementsByClassName('special')
+document.getElementsByClassName('special')[0]
+document.querySelector('.special')
+document.querySelectorAll('.special')[0]
+document.getElementsByTagName('p')
+document.getElementsByTagName('p')[0]
+document.querySelector('p')
+document.querySelectorAll('p')[0]
+document.querySelector('h1 + p')
