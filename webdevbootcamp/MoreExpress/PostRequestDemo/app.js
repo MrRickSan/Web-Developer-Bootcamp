ebeaba4f189be22bf7500293468acfccbd00dfad
Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-06-07 23:36:22 -0300

    Post requests

---
+++ webdevbootcamp/MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+var friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily']
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.post('/addfriend', function (req, res) {
+  var newFriend = req.body.newfriend
+  friends.push(newFriend)
+  res.redirect('/friends')
+})
+
+app.get('/friends', function (req, res) {
+  res.render('friends', {friends: friends})
+})
+
+app.listen(3000, function () {
+  console.log('Server started!!!')
+})
